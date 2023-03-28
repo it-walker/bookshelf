d1525ee014362c53e8c8c77424ec0eeec1bdd6be
h.sato <h.sato@sasukefinlab.com>
2023-03-29 08:38:40 +0900

    HAN-343

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,31 @@ import BookSearchDialog from './components/books/BookSearchDialog'
 import BookRow from './components/books/BookRow'
 import { BookDescription } from './components/books/BookDescription'
 import { BookToRead } from './components/books/BookToRead'
+import '@aws-amplify/ui-react/styles.css'
+import '@fontsource/inter/variable.css'
+import {Button, ThemeProvider} from "@aws-amplify/ui-react";
+
 
 Modal.setAppElement('#root')
+
+const theme = {
+  name: 'custom-button-theme',
+  tokens: {
+    components: {
+      button: {
+        // this will affect the font weight of all Buttons
+        fontWeight: { value: '{fontWeights.black.value}' },
+        // this will only style Buttons which are the "primary" variation
+        primary: {
+          backgroundColor: { value: 'rebeccapurple' },
+          _hover: {
+            backgroundColor: { value: 'hotpink' },
+          },
+        },
+      },
+    },
+  },
+}
 
 const customStyles = {
   overlay: {
@@ -81,18 +104,29 @@ const App = () => {
   })
 
   return (
-    <div className="App">
-      <section className="nav">
-        <h1>読みたい本リスト</h1>
-        <div className="button-like" onClick={handleAddClick}>本を追加</div>
-      </section>
-      <section className="main">
-        {bookRows}
-      </section>
-      <Modal isOpen={modalIsOpen} onRequestClose={handleModalClose} style={customStyles} >
-        <BookSearchDialog maxResults={20} onBookAdd={(b) => handleBookAdd(b)} />
-      </Modal>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <div>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+              href="https://fonts.googleapis.com/css2?family=Inter:slnt,wght@-10..0,100..900&display=swap"
+              rel="stylesheet"
+          />
+        </div>
+        <section className="nav">
+          <h1>読みたい本リスト</h1>
+          {/*<div className="button-like" onClick={handleAddClick}>本を追加</div>*/}
+          <Button variation="primary" onClick={handleAddClick}>本を追加</Button>
+        </section>
+        <section className="main">
+          {bookRows}
+        </section>
+        <Modal isOpen={modalIsOpen} onRequestClose={handleModalClose} style={customStyles} >
+          <BookSearchDialog maxResults={20} onBookAdd={(b) => handleBookAdd(b)} />
+        </Modal>
+      </div>
+    </ThemeProvider>
   )
 }
 
